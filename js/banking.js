83d6db37ddce93d03a480705db860d11ffb2dea0
akashwebs <aksah.shil227@gmail.com>
2022-02-11 17:30:07 +0600

    first commit

---
+++ js/banking.js
@@ -0,0 +1,66 @@
+//get input data convert to float 
+function getInputData(idName) {
+    const inputValue = document.getElementById(idName);
+    const inputAmount = parseFloat(inputValue.value)
+    inputValue.value = '';
+    return inputAmount;
+}
+
+// deposit and withdraw display amount
+function displayAmount(idName, newAmount) {
+    const amount = document.getElementById(idName);
+    const preveousAmount = parseFloat(amount.innerText);
+    const totalAmount = preveousAmount + newAmount;
+    amount.innerText = totalAmount.toFixed(2);
+}
+
+// balnce function deposit and withdraw input calculation
+
+function getDisplayPreveousTotal() {
+    const totalBalance = document.getElementById('total-balance');
+    const oldBalanceAmount = parseFloat(totalBalance.innerText)
+    return oldBalanceAmount;
+}
+
+function displayBalnce(amount, isTrue) {
+    const totalBalance = document.getElementById('total-balance');
+
+    const oldBalanceAmount = getDisplayPreveousTotal();
+
+    if (isTrue == true) {
+
+        totalBalance.innerText = (oldBalanceAmount + amount).toFixed(2);
+    } else {
+        totalBalance.innerText = (oldBalanceAmount - amount).toFixed(2);
+
+    }
+}
+
+// deposti handle
+document.getElementById('deposit-button').addEventListener('click', function() {
+    // 
+    // get input data of deposit
+    const depositInputamount = getInputData('deposit-input');
+
+    if (depositInputamount > 0) {
+        displayAmount('total-deposit', depositInputamount);
+        // blance add with desposit input
+        displayBalnce(depositInputamount, true);
+    }
+
+})
+
+// withdaraw handle
+document.getElementById('withdraw-button').addEventListener('click', function() {
+
+    // get withdaraw input 
+    const newWithdrawAmount = getInputData('withdraw-input');
+    const preveousDisplayAmount = getDisplayPreveousTotal();
+    if (newWithdrawAmount < preveousDisplayAmount) {
+        displayAmount('total-withdraw', newWithdrawAmount);
+        // blance minus from withdarw
+        displayBalnce(newWithdrawAmount, false);
+
+    }
+
+})
